f424d927bbc9a79165e871bec1692361d22f8c49
Patryk-Kowalczyk <tojestkowala@gmail.com>
2021-04-19 23:51:31 +0200

    Stepper styling

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,22 +1,34 @@
-import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
+    },
+    white: {
+      main: "#FFF",
     },
   },
 });
+
+declare module "@material-ui/core/styles/createPalette" {
+  interface Palette {
+    white: Palette["primary"];
+  }
+  interface PaletteOptions {
+    white: PaletteOptions["primary"];
+  }
+}
 
 export default theme;
